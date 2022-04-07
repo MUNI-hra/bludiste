207bcebb4963e4121f55885aba375f67e67215d0
OldaLP <78831891+OldaLP@users.noreply.github.com>
2022-04-07 17:25:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-import pygame
-import math
+import pygame, sys
+from pygame.locals import *
 
+pygame.init()
+DISPLAY_SURFACE = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Hra')
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        pygame.display.update()

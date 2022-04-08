664ebdef9d9a202e1dc00829e5ecc5a6f1b7ebd7
zidlinxfarben <94965824+zidlinxfarben@users.noreply.github.com>
2022-04-08 08:42:41 +0200

    added class Person and some comments

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import pygame, sys
 from pygame.locals import *
 
+class Person:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def move_y(self, ya):
+        if function(self.x, (self.y+ya)):  # is there a wall? (need this func.)
+            self.y += ya  # moving on y
+
+    def move_x(self, xa):
+        if function((self.x + xa), self.y):  # is there a wall? (need this func.)
+            self.x += xa  # moving on x
+
+    def end_of_word(self, max_x, max_y):  # border of window, can be done via constant
+        if self.x == max_x:  # right conner
+            return self.x, self.y
+        if self.x == 0:  # left conner
+            return self.x, self.y
+        if self.y == max_y:  # down conner
+            return self.x, self.y
+        if self.y == 0:  # up conner
+            return self.x, self.y
+
+
 pygame.init()
-DISPLAY_SURFACE = pygame.display.set_mode((400, 300))
+DISPLAY_SURFACE = pygame.display.set_mode((400, 300))  # need this numbers as variables or constants
 pygame.display.set_caption('Hra')
 while True:
     for event in pygame.event.get():
